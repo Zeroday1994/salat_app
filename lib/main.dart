ba061124f1cd769e1c1ffbea9b9893a963d4448a
Zeroday1994 <eng.a7med.ussef94@gmail.com>
2025-06-12 18:54:24 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
-// Main entry point of the app
-void main() => runApp(MyApp());
-import 'package:your_app/screens/prayer_times_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:salat_app/screens/prayer_times_screen.dart';
 
-// ثم استدعاء الصفحة عند التنقل إليها
-Navigator.push(context,
-    MaterialPageRoute(builder: (context) => PrayerTimesScreen()));
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'تطبيق الصلاة',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Arial',
+      ),
+      home: PrayerTimesScreen(),
+    );
+  }
+}
