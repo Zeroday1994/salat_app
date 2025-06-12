e6d497bec5e8336abc98669848a84f3e9e954f95
Zeroday1994 <eng.a7med.ussef94@gmail.com>
2025-06-12 17:10:28 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,7 @@
 // Main entry point of the app
 void main() => runApp(MyApp());
+import 'package:your_app/screens/prayer_times_screen.dart';
+
+// ثم استدعاء الصفحة عند التنقل إليها
+Navigator.push(context,
+    MaterialPageRoute(builder: (context) => PrayerTimesScreen()));
